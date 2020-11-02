f4686698ecfcc63067a92cc06595a4e7c3bf9775
magdmagd <dv.aboelmagd@gmail.com>
2020-11-03 00:27:24 +0200

    Simple Node Module Import

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-var rect = 
-{
-	perimeter: (x, y) => (2*(x+y)),
-	area: (x, y) => (x*y)
-};//end rect
+var rect = require('./reactangle');
+
 
 function solveRect(l,b) 
 {
@@ -16,7 +13,7 @@ function solveRect(l,b)
 	    console.log("The area of the rectangle is " + rect.area(l,b));
 	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
     }//end else
-}//end function solveRect(l,b) 
+}//end function solveRect(l,b)
 
 solveRect(2,4);
 solveRect(3,5);
